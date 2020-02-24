4fc3cdee10de68450daeedb111acbc014413d7d1
alextoson <alex@toson.com.au>
2020-02-24 21:32:03 +1100

    Made a tweak to the hero slider to only trigger when at top of the page

--- src/components/HeroSlider.js
+++ src/components/HeroSlider.js
@@ -1,4 +1,5 @@
 import React from "react";
+import debounce from "lodash";
 import Slider from "react-slick";
 import Slide from "./Slide";
 
@@ -7,22 +8,28 @@ export default class HeroSlider extends React.Component {
     lock: false
   };
   slide = event => {
-    if (!this.state.lock) {
-      event.preventDefault();
+    const scrollPosition = document.querySelector("html").scrollTop;
+    console.log("state:", this.state.lock);
+    if (scrollPosition === 0) {
+      if (!this.state.lock) {
+        event.preventDefault();
+        event.stopPropagation();
+      }
+      const delta = event.deltaY;
+      console.log(delta);
+      delta > 0 ? this.slider.slickNext() : this.slider.slickPrev();
     }
-    const delta = event.deltaY;
-    delta > 0 ? this.slider.slickNext() : this.slider.slickPrev();
   };
-  componentWillMount() {
-    window.addEventListener("wheel", e => {
-      this.slide(e);
-    });
-  }
-  componentWillUnmount() {
-    window.removeEventListener("wheel", e => {
-      this.slide(e);
-    });
-  }
+  // componentWillMount() {
+  //   // window.addEventListener("wheel", e => {
+  //   //   this.slide(e);
+  //   // });
+  // }
+  // componentWillUnmount() {
+  //   // window.removeEventListener("wheel", e => {
+  //   //   this.slide(e);
+  //   // });
+  // }
   render() {
     const settings = {
       arrows: false,
@@ -34,20 +41,27 @@ export default class HeroSlider extends React.Component {
       verticalSwiping: true,
       beforeChange: (currentSlide, nextSlide) => {
         console.log("before change", currentSlide, nextSlide);
-        if (nextSlide !== currentSlide) {
-          console.log("hello alex");
+        if (nextSlide > currentSlide || nextSlide < currentSlide) {
           this.setState(() => ({
-            lock: true
+            lock: false
           }));
         } else {
           this.setState(() => ({
-            lock: false
+            lock: true
           }));
         }
       }
     };
     return (
-      <section className="hero-slider">
+      <section
+        className="hero-slider"
+        onMouseOver={window.addEventListener("wheel", e => {
+          this.slide(e);
+        })}
+        onMouseLeave={window.removeEventListener("wheel", e => {
+          this.slide(e);
+        })}
+      >
         <Slider {...settings} ref={slider => (this.slider = slider)}>
           <Slide index={1} title="Our Services" />
           <Slide index={2} title="Brand Strategy" />
